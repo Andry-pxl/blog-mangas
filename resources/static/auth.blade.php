59207d78846924ac33d999729ca509fdd4151d18
RandyRagnar <randytsiory@gmail.com>
2024-10-16 02:20:00 +0300

    add

--- resources/static/auth.blade.php
+++ resources/static/auth.blade.php
@@ -22,7 +22,7 @@
             <div class="px-6 py-12 bg-white shadow sm:rounded-lg sm:px-12">
                 <form class="space-y-6" action="{{ $action }}" method="POST">
                     <div>
-                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Your email address</label>
                         <div class="mt-2">
                             <input id="email" name="email" type="email" autocomplete="email" required class="form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
