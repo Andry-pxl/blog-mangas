27d57812e3dba309ca2686efcdeeeebe9e6491e7
RandyRagnar <randytsiory@gmail.com>
2024-10-16 22:33:12 +0300

    two my commit

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ composer install
 php artisan optimize:clear
 php artisan config:clear
 php artisan migrate --force
-php artisan storage:link
+
 
 npm install
 npm run build
+
+php artisan storage:link
